1702cfdd4d441c3b29b3d302e7690d52a29e7846
hugosenari <hugosenari@gmail.com>
2022-03-12 00:53:53 -0300

    chore: re index breeze

---
+++ v0_0_4/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A dsl for writing macros in Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-breeze-v0_0_4.flake = false;
+  inputs.src-breeze-v0_0_4.owner = "alehander42";
+  inputs.src-breeze-v0_0_4.ref   = "refs/tags/v0.0.4";
+  inputs.src-breeze-v0_0_4.repo  = "breeze";
+  inputs.src-breeze-v0_0_4.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-breeze-v0_0_4"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-breeze-v0_0_4";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
