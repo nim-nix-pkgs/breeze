c45f2d4bfe2ed266e20bf6d260b352c746299110
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:15 -0300

    chore: re index breeze

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."breeze-master".dir   = "master";
+  inputs."breeze-master".owner = "nim-nix-pkgs";
+  inputs."breeze-master".ref   = "master";
+  inputs."breeze-master".repo  = "breeze";
+  inputs."breeze-master".type  = "github";
+  inputs."breeze-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."breeze-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."breeze-v0_0_4".dir   = "v0_0_4";
+  inputs."breeze-v0_0_4".owner = "nim-nix-pkgs";
+  inputs."breeze-v0_0_4".ref   = "master";
+  inputs."breeze-v0_0_4".repo  = "breeze";
+  inputs."breeze-v0_0_4".type  = "github";
+  inputs."breeze-v0_0_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."breeze-v0_0_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
